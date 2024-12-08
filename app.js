41fe5e3e645b7794dc0355c7288526ffd6e2b7da
dlyder70 <dlyder@outlook.com>
2024-12-08 18:55:50 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express'); // bruker node.js express web 
-app framework 
+const express = require('express'); // bruker node.js express web app framework 
 const app = express(); // get app instance (express function) 
 //env var PORT hentes inn fra fila .env som definert under scripts 
-/ start 
-//dette er verdien som hentes av process.env.PORT 
+// start
+// dette er verdien som hentes av process.env.PORT 
 const port = process.env.PORT || '3333'; //default til port 3333 
 function announceTime(clock) { 
 clock = clock || Date // Unix epoch 
@@ -15,19 +14,16 @@ return 'Applikasjonen startet ' + convdate
 var clockNow = announceTime(); 
 app.get('/', (req, res) => { 
 var accessedBy = req.ip.split(":").pop(); 
-res.send(clockNow + '. <br/><br/>Kalkulator på <a 
-href="./timeconv">/timeconv</a> .<br/>UnixEpochTime på <a 
-href="./unixepoch">/unixepoch</a> .<br><br>Aksessert fra ' + 
-accessedBy); 
+res.send(clockNow + '. <br/><br/>Kalkulator på <a href="./timeconv">/timeconv</a> .<br/>UnixEpochTime på <a href="./unixepoch">/unixepoch</a> .<br><br>Aksessert fra ' + accessedBy); 
 }); 
 function announceUxEpoch(clock) { 
-clock = clock || Date 
-var uxEpoch = clock.now() 
-return 'Unix epoch: ' + uxEpoch + ' (ms)' 
+    clock = clock || Date 
+    var uxEpoch = clock.now() 
+    return 'Unix epoch: ' + uxEpoch + ' (ms)' 
 } 
 var uxEpochNow = announceUxEpoch(); 
 app.get('/unixepoch', (req, res) => { 
-res.send(uxEpochNow); 
+    res.send(uxEpochNow); 
 }); 
 app.get('/timeconv', (req, res)=>{ 
 res.sendFile(__dirname + "/timeconv/" + "timeconv.html"); 
